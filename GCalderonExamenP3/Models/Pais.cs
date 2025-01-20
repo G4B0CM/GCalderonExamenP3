8e12936b0e57a766809207795b41116ffd966c88
laboratorio <laboratorio@UPOAULA10213>
2025-01-20 07:15:50 -0500

    Agregar archivos de proyecto.

---
+++ GCalderonExamenP3/Models/Pais.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace GCalderonExamenP3.Models
+{
+    [Table ("Pais")]
+    public class Pais
+    {
+        [PrimaryKey]
+        public string Nombre { get; set; }
+        public string Region { get; set; }
+        public string LinkGoogle { get; set; }
+        public string MiNombre { get; set; }
+    }
+}
